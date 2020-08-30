ed2c6e9a1323bbaaffa882a45d36b5064dfa04dc
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-08-30 22:10:52 +0900

    slack-ipaddr.sh

--- slack-ipaddr.sh
+++ slack-ipaddr.sh
@@ -11,6 +11,12 @@ export HEAD="[IPアドレス通知] `hostname`\n"
 
 TMP_FILE=`mktemp`
 
+IPADDR=""
+while [ -z "$IPADDR" ]; do
+    sleep 1
+    IPADDR=`ifconfig -a | grep inet | grep -v inet6 | grep -v '127.0.0.1' | sed 's/^ *//' | cut -d ' ' -f 2`
+done
+
 date +'%F %T %Z' > $TMP_FILE
 echo >> $TMP_FILE
 echo -n "IP addrs: " >> $TMP_FILE
